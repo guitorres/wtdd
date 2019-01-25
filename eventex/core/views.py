2700e071c0110390b93c6a75903690d67651a11c
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2019-01-25 12:44:54 -0200

    Adiciona parcialmente palestrantes

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  speakers = [
-    {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
-    {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
-  ]
-  return render(request, 'index.html', {'speakers': speakers})
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+    ]
+    return render(request, 'index.html', {'speakers': speakers})
+
+
+def speaker_detail(request, slug):
+    return render(request, 'core/speaker_detail.html')
