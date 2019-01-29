bd5349aa5c2a55e4ff2dfbdb5f2a299d72435a31
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2019-01-29 09:36:10 -0200

    Ajusta teste.

--- eventex/core/tests/test_model_speaker.py
+++ eventex/core/tests/test_model_speaker.py
@@ -1,12 +1,12 @@
-# from django.test import TestCase
-# from eventex.core.models import Speaker
+from django.test import TestCase
+from eventex.core.models import Speaker
 
-# class SpeakerModelTest(TestCase):
-#     def test_create(self):
-#         speaker = Speaker.objects.create(
-#             name = 'Grace Hopper',
-#             slug = 'grace-hopper',
-#             photo = 'http://hbn.link/hopper-pic',
-#             description = 'Programadora e almirante.'
-#         )
-#         self.assertTrue(Speaker.objects.exists())
+class SpeakerModelTest(TestCase):
+    def test_create(self):
+        Speaker.objects.create(
+            name = 'Grace Hopper',
+            slug = 'grace-hopper',
+            photo = 'http://hbn.link/hopper-pic',
+            description = 'Programadora e almirante.'
+        )
+        self.assertTrue(Speaker.objects.exists())
