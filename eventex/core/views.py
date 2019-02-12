36c70d66bc00f515c9e4b95e54045450bda44847
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2019-02-12 08:55:52 -0200

    Palestras

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from eventex.core.models import Speaker
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,7 @@ def home(request):
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
     return render(request, 'core/speaker_detail.html', {'speaker': speaker})
+
+
+def talk_list(request):
+    return render(request, 'core/talk_list.html')
