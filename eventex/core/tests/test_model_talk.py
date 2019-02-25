3f1307c31e5857f1bb94a0273276a722483270c7
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2019-02-25 09:01:21 -0300

    Teste talk list

---
+++ eventex/core/tests/test_model_talk.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from eventex.core.models import Talk
+
+class TestModelTalk(TestCase):
+    def setUp(self):
+        self.talk = Talk.objects.create(
+            title='Título da Palestra',
+            start='10:00',
+            description='Descrição da palestra.',
+        )
+
+    def test_create(self):
+        self.assertTrue(Talk.objects.exists())
--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,8 +1,18 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from eventex.core.models import Talk
 
 class TestTalkList(TestCase):
     def setUp(self):
+        Talk.objects.create(
+            title='Título da Palestra', start='10:00',
+            description='Descrição da palestra.'
+        )
+        Talk.objects.create(
+            title='Título da Palestra', start='13:00',
+            description='Descrição da palestra.'
+        )
+
         self.resp = self.client.get(r('talk_list'))
 
     def test_get(self):
@@ -10,3 +20,22 @@ class TestTalkList(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'core/talk_list.html')
+
+    def test_html(self):
+        contents = [
+            (2, 'Título da Palestra'),
+            (1, '10:00'),
+            (1, '13:00'),
+            (2, '/palestrantes/henrique-bastos/'),
+            (2, 'Henrique Bastos'),
+            (2, 'Descrição da palestra.'),
+        ]
+        for count, expected in contents:
+            with self.subTest():
+                self.assertContains(self.resp, expected, count)
+
+    def text_context(self):
+        variables = ['morning_talks', 'afternoon_talks']
+        for key in variables:
+            with self.subTest():
+                self.assertContains(key, self.resp.context)
